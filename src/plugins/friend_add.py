0e689482a0b09e412a471532f3ef1dc2e4a98c83
wyf7685 <wyf7685@163.com>
2024-06-02 23:55:45 +0800

    feat: new plugin `friend_add`

---
+++ src/plugins/friend_add.py
@@ -0,0 +1,22 @@
+from nonebot import get_driver, on_notice, require
+from nonebot.adapters import Event
+from nonebot.adapters.onebot.v11.event import FriendAddNoticeEvent
+
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_userinfo")
+from nonebot_plugin_alconna.uniseg import Target, UniMessage
+from nonebot_plugin_userinfo import EventUserInfo, UserInfo
+
+
+def is_friend_add(event: Event):
+    return isinstance(event, FriendAddNoticeEvent)
+
+
+@on_notice(rule=is_friend_add).handle()
+async def _(info: UserInfo = EventUserInfo()):
+    message = UniMessage.text(f"收到好友申请: {info.user_name}({info.user_id})\n")
+    if avatar := info.user_avatar:
+        message += UniMessage.image(raw=await avatar.get_image())
+
+    for target in get_driver().config.superusers:
+        await message.send(Target(target, private=True))
