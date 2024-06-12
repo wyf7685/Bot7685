9046d4949cbe7f865c0cffb12d41c4221b897159
wyf7685 <wyf7685@163.com>
2024-06-13 00:17:34 +0800

    修改日志文件的记录等级为DEBUG

--- bot.py
+++ bot.py
@@ -1,25 +1,20 @@
 import nonebot
-# from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-from nonebot.adapters.qq import Adapter as QQAdapter
-from nonebot.log import default_filter, default_format, logger
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+from nonebot.log import default_format, logger
 
 logger.add(
     "./logs/{time:YYYY-MM-DD}.log",
     rotation="00:00",
-    level=0,
+    level="DEBUG",
     diagnose=False,
-    filter=default_filter,
     format=default_format,
 )
 
 nonebot.init()
-
 driver = nonebot.get_driver()
-# driver.register_adapter(ONEBOT_V11Adapter)
-driver.register_adapter(QQAdapter)
-
-
+driver.register_adapter(ONEBOT_V11Adapter)
 nonebot.load_from_toml("pyproject.toml")
+
 
 if __name__ == "__main__":
     nonebot.run()
