55daea87a9ab2de55599cbc9a9d4f2f42b48c745
wyf7685 <wyf7685@163.com>
2024-06-13 22:53:36 +0800

    测试Console适配器

--- bot.py
+++ bot.py
@@ -1,17 +1,25 @@
 import nonebot
+from nonebot.adapters.console import Adapter as ConsoleAdapter
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-from nonebot.log import default_format, logger
+from nonebot.log import logger
 
 logger.add(
     "./logs/{time:YYYY-MM-DD}.log",
     rotation="00:00",
     level="DEBUG",
     diagnose=False,
-    format=default_format,
+    format=(
+        "<g>{time:MM-DD HH:mm:ss}</g> "
+        "[<lvl>{level}</lvl>] "
+        "<c><u>{name}</u></c> | "
+        "<c>{function}:{line}</c>| "
+        "{message}"
+    ),
 )
 
 nonebot.init()
 driver = nonebot.get_driver()
+driver.register_adapter(ConsoleAdapter)
 driver.register_adapter(ONEBOT_V11Adapter)
 nonebot.load_from_toml("pyproject.toml")
 
