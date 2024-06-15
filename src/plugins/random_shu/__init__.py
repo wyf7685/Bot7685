5e7733f4d0bec2ba1d4697065d65ebc88684fbf0
wyf7685 <wyf7685@163.com>
2024-06-15 21:23:47 +0800

    黍泡泡: 优化代码(并没有

--- src/plugins/random_shu/__init__.py
+++ src/plugins/random_shu/__init__.py
@@ -10,9 +10,6 @@ url = setup_router()
 @on_startswith(("抽黍泡泡", "黍泡泡")).handle()
 async def _(bot: Bot, event: MessageEvent):
     fp = random.choice(image_fps)
-
     img = MessageSegment.image(file=str(url.with_query({"key": fp.name})))
     img.data["summary"] = image_text.get(fp.stem, "黍泡泡")
-    msg = MessageSegment.reply(event.message_id) + img
-
-    await bot.send(event, msg)
+    await bot.send(event, MessageSegment.reply(event.message_id) + img)
