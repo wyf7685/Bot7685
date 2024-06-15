6f3668388f7a9712dfecbfbe3fcb44b879646531
wyf7685 <wyf7685@163.com>
2024-06-15 21:23:25 +0800

    御神签: 将依赖注入拆分为单个文件

--- src/plugins/lots/__init__.py
+++ src/plugins/lots/__init__.py
@@ -1,50 +1,19 @@
-from typing import Annotated
-
 from nonebot import require
-from nonebot.adapters import Bot, Event
+from nonebot.adapters import Bot
 from nonebot.log import logger
-from nonebot.params import Depends
 
 require("nonebot_plugin_alconna")
-from arclet.alconna import Alconna, Args
-from nonebot_plugin_alconna import AlcMatches, on_alconna
+from nonebot_plugin_alconna import Alconna, Args, on_alconna
 from nonebot_plugin_alconna.uniseg import At, UniMessage
 
+from .depends import LotsTarget, MsgId
 from .lots_data import get_lots_msg
 
 lots = on_alconna(Alconna("御神签", Args["target?", At]))
 
 
-def LotsTarget():
-
-    def lots_target(event: Event, result: AlcMatches) -> str:
-        uin = event.get_user_id()
-        if target := result.query[At]("target"):
-            uin = target.target
-        return uin
-
-    return Depends(lots_target)
-
-
-def MsgId():
-    try:
-        from nonebot.adapters.onebot.v11 import MessageEvent
-    except ImportError:
-        MessageEvent = None
-
-    def msg_id(event: Event) -> int | None:
-        if MessageEvent is not None and isinstance(event, MessageEvent):
-            return event.message_id
-
-    return Depends(msg_id)
-
-
 @lots.handle()
-async def _(
-    bot: Bot,
-    target: Annotated[str, LotsTarget()],
-    msgid: Annotated[int | None, MsgId()],
-):
+async def _(bot: Bot, target: LotsTarget, msgid: MsgId):
     msg, emoji = get_lots_msg(target)
     await UniMessage.text(msg).send(reply_to=True)
 
---
+++ src/plugins/lots/depends.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+
+from nonebot.adapters import Event
+from nonebot.params import Depends
+from nonebot_plugin_alconna import AlcMatches
+from nonebot_plugin_alconna.uniseg import At
+
+
+def _LotsTarget():
+
+    async def lots_target(event: Event, result: AlcMatches) -> str:
+        uin = event.get_user_id()
+        if target := result.query[At]("target"):
+            uin = target.target
+        return uin
+
+    return Depends(lots_target)
+
+
+def _MsgId():
+    try:
+        from nonebot.adapters.onebot.v11 import MessageEvent
+    except ImportError:
+        MessageEvent = None
+
+    def msg_id(event: Event) -> int | None:
+        if MessageEvent is not None and isinstance(event, MessageEvent):
+            return event.message_id
+
+    return Depends(msg_id)
+
+
+LotsTarget = Annotated[str, _LotsTarget()]
+MsgId = Annotated[int | None, _MsgId()]
