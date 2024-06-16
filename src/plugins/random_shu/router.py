4ece0a39ab75e15e819d789915a6e79ae45a5838
wyf7685 <wyf7685@163.com>
2024-06-16 13:14:46 +0800

    黍泡泡: 日志记录抽取结果

--- src/plugins/random_shu/router.py
+++ src/plugins/random_shu/router.py
@@ -1,7 +1,8 @@
 from nonebot import get_driver
 from nonebot.drivers import URL, ASGIMixin, HTTPServerSetup, Request, Response
+from nonebot.log import logger
 
-from .constant import image_dir, router_path, nonebot_config
+from .constant import image_dir, image_text, nonebot_config, router_path
 
 
 async def handle(request: Request) -> Response:
@@ -11,6 +12,9 @@ async def handle(request: Request) -> Response:
     fp = image_dir / key
     if not fp.exists():
         return Response(404, content="Requested key not exists")
+
+    image_text.get(fp.stem, "黍泡泡")
+    logger.info(f"黍泡泡抽取结果: {fp.name} - {image_text.get(fp.stem, '黍泡泡')}")
     return Response(
         200,
         headers={"Content-Type": "image/png"},
