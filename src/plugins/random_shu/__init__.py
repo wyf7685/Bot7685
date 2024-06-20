7490ffac269cda95873385630150e1ec35781aa0
wyf7685 <wyf7685@163.com>
2024-06-20 15:40:28 +0800

    黍泡泡: 移动事件定义

--- src/plugins/random_shu/__init__.py
+++ src/plugins/random_shu/__init__.py
@@ -1,43 +1,18 @@
 from datetime import timedelta
-from typing import Literal, override
-
+from typing import cast
 from nonebot import on_fullmatch, on_notice, on_startswith
-from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
 from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent, MessageSegment
-from nonebot.adapters.onebot.v11.event import GroupMessageEvent, NoticeEvent
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent
 from nonebot.log import logger
 from nonebot.matcher import Matcher
 from nonebot.typing import T_State
-from pydantic import BaseModel
 
 from .constant import emoji_weight_actions
 from .data import Data
+from .event import GroupMsgEmojiLikeEvent
 from .router import setup_router
 
 url = setup_router()
-
-
-class GroupMsgEmojiLikeEvent(NoticeEvent):
-    class Like(BaseModel):
-        emoji_id: str
-        count: int
-
-    notice_type: Literal["group_msg_emoji_like"]  # type: ignore
-    user_id: int
-    group_id: int
-    message_id: int
-    likes: list[Like]
-
-    @override
-    def get_user_id(self) -> str:
-        return str(self.user_id)
-
-    @override
-    def get_session_id(self) -> str:
-        return f"group_{self.group_id}_{self.user_id}"
-
-
-OneBotV11Adapter.add_custom_model(GroupMsgEmojiLikeEvent)
 
 
 def handle_response(msg_id: int, user_id: int, item: Data) -> type[Matcher]:
@@ -48,14 +23,13 @@ def handle_response(msg_id: int, user_id: int, item: Data) -> type[Matcher]:
         if event.message_id != msg_id:
             return False
 
-        is_event_user = event.user_id == user_id
-        cache: dict[int, int] = state.setdefault("like_cache", {})
-        likes: dict[int, int] = {int(i.emoji_id): i.count for i in event.likes}
+        cache = cast(dict[int, int], state.setdefault("like_cache", {}))
+        likes = {int(i.emoji_id): i.count for i in event.likes}
         for k, v in likes.items():
             if (
                 (k not in cache or cache[k] < v)  # 判断新增表情回应
-                and is_event_user  # 判断是否为触发黍泡泡的用户
-                and k in emoji_weight_actions  # 判断是否为指定表情
+                and (event.user_id == user_id)  # 判断是否为触发黍泡泡的用户
+                and (k in emoji_weight_actions)  # 判断是否为指定表情
             ):
                 state["emoji_id"] = k
                 return True
@@ -69,8 +43,10 @@ def handle_response(msg_id: int, user_id: int, item: Data) -> type[Matcher]:
         weight, action = emoji_weight_actions.get(state["emoji_id"], (0, ""))
         if weight:
             item.add_weight(weight)
-            msg = f"黍泡泡 [<le>{item.text}</le>] 权重{action} {abs(weight)}, 当前权重: <c>{item.weight}</c>"
-            logger.opt(colors=True).info(msg)
+            logger.opt(colors=True).info(
+                f"黍泡泡 [<le>{item.text}</le>] 权重{action} {abs(weight)}, "
+                f"当前权重: <c>{item.weight}</c>"
+            )
 
     return on_notice(
         rule=rule,
@@ -100,3 +76,4 @@ async def _(bot: Bot, event: MessageEvent):
         # 暂时使用该方法纠正, 等 NapCatQQ 修复后再修改
         matcher = handle_response(send_result["message_id"] + 1, event.user_id, item)
         logger.debug(f"创建 Callback: {matcher}")
+        matcher.got
