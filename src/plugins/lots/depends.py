b8bcc027332765b14776d8073a64b6d9dc91ab06
wyf7685 <wyf7685@163.com>
2024-06-23 01:14:06 +0800

    御神签: 优化

--- src/plugins/lots/depends.py
+++ src/plugins/lots/depends.py
@@ -6,17 +6,6 @@ from nonebot_plugin_alconna import AlcMatches
 from nonebot_plugin_alconna.uniseg import At
 
 
-def _LotsTarget():
-
-    async def lots_target(event: Event, result: AlcMatches) -> str:
-        uin = event.get_user_id()
-        if target := result.query[At]("target"):
-            uin = target.target
-        return uin
-
-    return Depends(lots_target)
-
-
 def _MsgId():
     try:
         from nonebot.adapters.onebot.v11 import MessageEvent
@@ -30,5 +19,16 @@ def _MsgId():
     return Depends(msg_id)
 
 
-LotsTarget = Annotated[str, _LotsTarget()]
+def _LotsTarget():
+
+    async def lots_target(event: Event, result: AlcMatches) -> str:
+        uin = event.get_user_id()
+        if target := result.query[At]("target"):
+            uin = target.target
+        return uin
+
+    return Depends(lots_target)
+
+
 MsgId = Annotated[Optional[int], _MsgId()]
+LotsTarget = Annotated[str, _LotsTarget()]
