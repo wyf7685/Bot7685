1a51b6efcded8fce8b5940d03e359d6c0cbe3787
wyf7685 <wyf7685@163.com>
2024-07-09 21:58:23 +0800

    code: ob11导出常用函数

--- src/plugins/exe_code/interface/adapter_api/onebot11.py
+++ src/plugins/exe_code/interface/adapter_api/onebot11.py
@@ -3,7 +3,7 @@ from typing import ClassVar
 from ..api import API as BaseAPI
 from ..api import register_api
 from ..help_doc import descript
-from ..utils import Result, debug_log
+from ..utils import Result, debug_log, export
 
 try:
     from nonebot.adapters.onebot.v11 import Bot
@@ -12,6 +12,7 @@ try:
     class API(BaseAPI):
         __inst_name__: ClassVar[str] = "api"
 
+        @export
         @descript(
             description="[OneBotV11] 撤回指定消息",
             parameters=dict(msg_id="需要撤回的消息ID，可通过Result获取"),
@@ -20,6 +21,7 @@ try:
         async def recall(self, msg_id: int) -> Result:
             return await self.call_api("delete_msg", message_id=msg_id)
 
+        @export
         @descript(
             description="[OneBotV11] 通过消息ID获取指定消息",
             parameters=dict(msg_id="需要获取的消息ID，可通过getmid获取"),
@@ -28,6 +30,7 @@ try:
         async def get_msg(self, msg_id: int) -> Result:
             return await self.call_api("get_msg", message_id=msg_id)
 
+        @export
         @descript(
             description="[OneBotV11] 通过合并转发ID获取合并转发消息",
             parameters=dict(msg_id="需要获取的合并转发ID，可通过getcqcode获取"),
