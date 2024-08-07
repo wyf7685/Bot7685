6626714e65d2dc3f708160382d47a91baf613657
wyf7685 <wyf7685@163.com>
2024-08-07 12:38:24 +0800

    重构 read_60s

---
+++ src/plugins/read_60s.py
@@ -0,0 +1,61 @@
+import contextlib
+
+import httpx
+from nonebot import get_bots, get_plugin_config, require
+from nonebot.adapters.onebot.v11 import Bot, Message, MessageSegment
+from pydantic import BaseModel, Field
+
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
+
+
+class Config(BaseModel):
+    class Time(BaseModel):
+        hour: int = Field(default=0, alias="HOUR")
+        minute: int = Field(default=0, alias="MINUTE")
+
+    read_qq_friends: list[int] = Field(default_factory=list)
+    read_qq_groups: list[int] = Field(default_factory=list)
+    read_inform_time: list[Time] = Field(default_factory=list)
+
+
+config = get_plugin_config(Config)
+api_url = ["https://api.2xb.cn/zaob", "https://api.iyk0.com/60s"]
+
+
+async def get_url(api: str) -> str:
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(api)
+        resp.raise_for_status()
+    return resp.json()["imageUrl"]
+
+
+async def suijitu():
+    for api in api_url:
+        with contextlib.suppress(Exception):
+            url = await get_url(api)
+            break
+    else:
+        return Message(MessageSegment.text("今日60S读世界获取失败!"))
+
+    return "今日60S读世界已送达\n" + MessageSegment.image(url)
+
+
+async def read60s():
+    msg = await suijitu()
+    bot = next(b for b in get_bots().values() if isinstance(b, Bot))
+
+    for qq in config.read_qq_friends:
+        await bot.send_private_msg(user_id=qq, message=msg)
+
+    for qq_group in config.read_qq_groups:
+        await bot.send_group_msg(group_id=qq_group, message=msg)
+
+
+for time in config.read_inform_time:
+    scheduler.add_job(
+        read60s,
+        "cron",
+        hour=time.hour,
+        minute=time.minute,
+    )
