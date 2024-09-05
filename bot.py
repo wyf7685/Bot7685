05fe54106a748de3f4310b566046b009b15ecbac
wyf7685 <wyf7685@163.com>
2024-09-05 16:46:26 +0800

    自定义插件加载策略

--- bot.py
+++ bot.py
@@ -19,13 +19,36 @@ logger.add(
     ),
 )
 
+
+def custom_load():
+    import pathlib
+    import tomllib
+
+    with open("pyproject.toml", encoding="utf-8") as f:
+        data = tomllib.loads(f.read())
+    nonebot_data = data.get("tool", {}).get("nonebot")
+    plugins: list[str] = nonebot_data.get("plugins", [])
+
+    for p in pathlib.Path("src/dev").iterdir():
+        if (p.is_dir() and (name := p.name) in plugins) or (
+            p.is_file() and p.suffix == ".py" and (name := p.stem) in plugins
+        ):
+            plugins.remove(name)
+            logger.opt(colors=True).warning(
+                f'优先加载来自 "<m>src.dev.{name}</m>" 的插件 "<y>{name}</y>"'
+            )
+
+    nonebot.load_all_plugins(plugins, ["src/plugins", "src/dev"])
+
+
 nonebot.init()
 app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(OB11Adapter)
 driver.register_adapter(QQAdapter)
 # driver.register_adapter(TelegramAdapter)
-nonebot.load_from_toml("pyproject.toml")
+# nonebot.load_from_toml("pyproject.toml")
+custom_load()
 
 
 if __name__ == "__main__":
