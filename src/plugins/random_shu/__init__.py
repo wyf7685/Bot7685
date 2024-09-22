c765a2a516703e98518f8086cc75d927ed4296e0
wyf7685 <wyf7685@163.com>
2024-09-22 13:41:29 +0800

    fix

--- src/plugins/random_shu/__init__.py
+++ src/plugins/random_shu/__init__.py
@@ -1,5 +1,6 @@
 import random
 from pathlib import Path
+from typing import NoReturn
 
 from nonebot import require
 
@@ -7,8 +8,9 @@ require("nonebot_plugin_alconna")
 from nonebot_plugin_alconna import Command, UniMessage
 
 images = list((Path(__file__).parent / "images").iterdir())
-matcher = (
-    Command("黍泡泡 [...args]")
-    .action(lambda: UniMessage.image(raw=random.choice(images).read_bytes()))
-    .build(priority=2)
-)
+matcher = Command("黍泡泡 [...args]").build(priority=2)
+
+
+@matcher.handle()
+async def _() -> NoReturn:
+    await UniMessage.image(raw=random.choice(images).read_bytes()).finish(reply_to=True)
