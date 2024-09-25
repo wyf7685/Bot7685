8ec5201d6ebccf96b7394200bb4aacff301cdf46
wyf7685 <wyf7685@163.com>
2024-09-25 13:29:59 +0800

    fix start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 
-if [ ! -f /bot7685.initialize.flag]; then
+if [ ! -f /bot7685.initialize.flag ]; then
   BOT7685_UID=${BOT7685_UID:=911}
   BOT7685_GID=${BOT7685_GID:=1001}
   usermod -o -u ${BOT7685_UID} bot7685
