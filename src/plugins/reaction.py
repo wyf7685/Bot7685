fa6409117d485a1bbd6cae58cf173a1589ad758c
wyf7685 <wyf7685@163.com>
2024-10-10 16:04:20 +0800

    fix

--- src/plugins/reaction.py
+++ src/plugins/reaction.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 from nonebot import get_plugin_config, on_keyword, on_message, require
 from nonebot.adapters import Bot, Event
 from pydantic import BaseModel
@@ -7,6 +9,13 @@ require("nonebot_plugin_session")
 from nonebot_plugin_alconna import MsgTarget
 from nonebot_plugin_session import EventSession
 
+if TYPE_CHECKING:
+    from nonebot_plugin_exe_code.interface import get_api_class
+else:
+    require("src.dev.nonebot_plugin_exe_code")
+    from src.dev.nonebot_plugin_exe_code.interface import (  # pyright:ignore[reportMissingImports]
+        get_api_class,
+    )
 
 class Config(BaseModel):
     reaction_users: set[str] = set()
@@ -24,10 +33,6 @@ bubble = on_keyword({"冒泡"}, _rule_bubble)
 
 @bubble.handle()
 async def handle_bubble(bot: Bot, event: Event, session: EventSession) -> None:
-    from src.dev.nonebot_plugin_exe_code.interface import (  # pyright:ignore[reportMissingImports]
-        get_api_class,
-    )
-
     api = get_api_class(bot)(bot, event, session, {})
     if set_reaction := getattr(api, "set_reaction", None):
         await set_reaction(38, api.mid)
@@ -42,10 +47,6 @@ matcher = on_message(rule=_rule, block=False)
 
 @matcher.handle()
 async def _(bot: Bot, event: Event, session: EventSession) -> None:
-    from src.dev.nonebot_plugin_exe_code.interface import (  # pyright:ignore[reportMissingImports]
-        get_api_class,
-    )
-
     api = get_api_class(bot)(bot, event, session, {})
     if set_reaction := getattr(api, "set_reaction", None):
         await set_reaction(424, api.mid)
