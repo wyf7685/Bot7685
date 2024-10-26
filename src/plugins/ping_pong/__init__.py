f65f96a42c3198fc358d3165c0d85c1ac166dc59
wyf7685 <wyf7685@163.com>
2024-10-26 16:39:52 +0800

    ping_pong: send raw file

--- src/plugins/ping_pong/__init__.py
+++ src/plugins/ping_pong/__init__.py
@@ -7,7 +7,9 @@ require("nonebot_plugin_alconna")
 from nonebot_plugin_alconna import Command, UniMessage
 
 Command("ping").action(
-    lambda: UniMessage.text("pong").image(
-        path=random.choice(list(pathlib.Path(__file__).resolve().parent.glob("*.jpg")))
+    func=lambda: UniMessage.text("pong").image(
+        raw=random.choice(
+            list(pathlib.Path(__file__).resolve().parent.glob("*.jpg"))
+        ).read_bytes()
     )
 ).build()
