63e19f0ac0f9a12dec43e9873b290cd8321a598c
wyf7685 <wyf7685@163.com>
2024-10-26 16:42:40 +0800

    update

--- src/plugins/ping_pong/__init__.py
+++ src/plugins/ping_pong/__init__.py
@@ -4,12 +4,19 @@ import random
 from nonebot import require
 
 require("nonebot_plugin_alconna")
-from nonebot_plugin_alconna import Command, UniMessage
+from nonebot_plugin_alconna import UniMessage, on_alconna
 
-Command("ping").action(
-    func=lambda: UniMessage.text("pong").image(
-        raw=random.choice(
-            list(pathlib.Path(__file__).resolve().parent.glob("*.jpg"))
-        ).read_bytes()
+ping = on_alconna("ping", priority=10)
+
+
+@ping.handle()
+async def _() -> None:
+    await (
+        UniMessage.text("pong")
+        .image(
+            raw=random.choice(
+                list(pathlib.Path(__file__).resolve().parent.glob("*.jpg"))
+            ).read_bytes()
+        )
+        .finish()
     )
-).build()
