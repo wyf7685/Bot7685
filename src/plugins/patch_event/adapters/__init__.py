d11a0601bc94474bb3cc791ce76a94daa3aecc5f
wyf7685 <wyf7685@163.com>
2024-11-06 13:41:50 +0800

    patch: telegram event

--- src/plugins/patch_event/adapters/__init__.py
+++ src/plugins/patch_event/adapters/__init__.py
@@ -2,3 +2,4 @@ from . import discord as discord
 from . import onebot11 as onebot11
 from . import qq as qq
 from . import satori as satori
+from . import telegram as telegram
---
+++ src/plugins/patch_event/adapters/telegram.py
@@ -0,0 +1,76 @@
+import contextlib
+from typing import override
+
+from nonebot.utils import escape_tag
+
+from ..patcher import Patcher
+
+with contextlib.suppress(ImportError):
+    from nonebot.adapters.telegram.event import (
+        GroupEditedMessageEvent,
+        GroupMessageEvent,
+        PrivateEditedMessageEvent,
+        PrivateMessageEvent,
+    )
+
+    @Patcher
+    class PatchGroupMessageEvent(GroupMessageEvent):
+        @override
+        def get_log_string(self) -> str:
+            nick = self.from_.first_name + (
+                f" {self.from_.last_name}" if self.from_.last_name else ""
+            )
+            chat = f"<c>{self.chat.id}</c>"
+            if self.chat.title:
+                chat = f"<y>{escape_tag(self.chat.title)}</y>({chat})"
+            return (
+                f"[{self.get_event_name()}]: "
+                f"Message <c>{self.message_id}</c> from "
+                f"<y>{escape_tag(nick)}</y>(<c>{self.from_.id}</c>)@[Chat {chat}]: "
+                f"{self.get_message_description()}"
+            )
+
+    @Patcher
+    class PatchPrivateMessageEvent(PrivateMessageEvent):
+        @override
+        def get_log_string(self) -> str:
+            nick = self.from_.first_name + (
+                f" {self.from_.last_name}" if self.from_.last_name else ""
+            )
+            return (
+                f"[{self.get_event_name()}]: "
+                f"Message <c>{self.message_id}</c> from "
+                f"<y>{escape_tag(nick)}</y>(<c>{self.from_.id}</c>): "
+                f"{self.get_message_description()}"
+            )
+
+    @Patcher
+    class PatchGroupEditedMessageEvent(GroupEditedMessageEvent):
+        @override
+        def get_log_string(self) -> str:
+            nick = self.from_.first_name + (
+                f" {self.from_.last_name}" if self.from_.last_name else ""
+            )
+            chat = f"<c>{self.chat.id}</c>"
+            if self.chat.title:
+                chat = f"<y>{escape_tag(self.chat.title)}</y>({chat})"
+            return (
+                f"[{self.get_event_name()}]: "
+                f"EditedMessage <c>{self.message_id}</c> from "
+                f"<y>{escape_tag(nick)}</y>(<c>{self.from_.id}</c>)@[Chat {chat}]: "
+                f"{self.get_message_description()}"
+            )
+
+    @Patcher
+    class PatchPrivateEditedMessageEvent(PrivateEditedMessageEvent):
+        @override
+        def get_log_string(self) -> str:
+            nick = self.from_.first_name + (
+                f" {self.from_.last_name}" if self.from_.last_name else ""
+            )
+            return (
+                f"[{self.get_event_name()}]: "
+                f"EditedMessage <c>{self.message_id}</c> from "
+                f"<y>{escape_tag(nick)}</y>(<c>{self.from_.id}</c>): "
+                f"{self.get_message_description()}"
+            )
