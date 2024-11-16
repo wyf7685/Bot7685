f9f6c238d5c371525c9703f01dd9cfb4cf900dc7
wyf7685 <wyf7685@163.com>
2024-11-16 19:26:09 +0800

    add

---
+++ src/plugins/tgsetu.py
@@ -0,0 +1,39 @@
+import httpx
+from nonebot import require
+from nonebot.adapters import Bot
+
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import Alconna, Match, Option, UniMessage, on_alconna
+
+
+async def _check(bot: Bot) -> bool:
+    return bot.type.lower() == "telegram"
+
+
+setu = on_alconna(
+    Alconna("setu", Option("r18")),
+    rule=_check,
+    aliases={"色图", "涩图", "来张涩图", "来张色图"},
+    use_cmd_start=True,
+)
+
+
+@setu.handle()
+async def _(r18: Match) -> None:
+    base_url = "https://api.lolicon.app/setu"
+    params = {"r18": int(r18.available)}
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(base_url, params=params)
+        if resp.status_code != 200:
+            await UniMessage.text("接口请求失败").finish(reply_to=True)
+        data = resp.json()
+        if data["code"] != 0:
+            await UniMessage.text(data["msg"]).finish(reply_to=True)
+
+        img_url = data["data"][0]["url"]
+        resp = await client.get(img_url)
+        if resp.status_code != 200:
+            await UniMessage.text("图片获取失败").finish(reply_to=True)
+        data = resp.read()
+
+    await UniMessage.image(raw=data).send()
