1c82b14c33542ce1ce30b47ed39ac032ef7fe8ef
wyf7685 <wyf7685@163.com>
2024-11-20 13:12:44 +0800

    ob11 bili share

--- src/plugins/group_pipe/processor/onebot11.py
+++ src/plugins/group_pipe/processor/onebot11.py
@@ -1,12 +1,23 @@
+import contextlib
+import json
 from collections.abc import AsyncGenerator
 from typing import Any, override
 
+import httpx
 from nonebot.adapters import Event
 from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent, MessageSegment
 from nonebot_plugin_alconna.uniseg import Image, Reply, Segment, Text
 
 from .common import MessageProcessor as BaseMessageProcessor
 from .common import download_file
+
+
+async def solve_url_302(url: str) -> str:
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        if resp.status_code == 302:
+            return resp.headers["Location"].partition("?")[0]
+        return url
 
 
 class MessageProcessor(BaseMessageProcessor[MessageSegment, Bot, Message]):
@@ -21,9 +32,24 @@ class MessageProcessor(BaseMessageProcessor[MessageSegment, Bot, Message]):
     async def extract_msg_id(msg_ids: list[dict[str, Any]]) -> str:
         return str(msg_ids[0]["message_id"]) if msg_ids else ""
 
-    def handle_json_msg(self, data: dict[str, Any]) -> Segment:
-        # todo: solve card share
-        return Text(f"[json消息:{data}]")
+    async def handle_json_msg(
+        self, data: dict[str, Any]
+    ) -> AsyncGenerator[Segment, None]:
+        def default() -> Segment:
+            return Text(f"[json消息:{data}]")
+
+        meta = data.get("meta", {})
+        if not meta:
+            yield default()
+
+        # Bilibili share
+        if "detail_1" in meta and meta["detail_1"]["title"] == "哔哩哔哩":
+            detail = meta["detail_1"]
+            url = await solve_url_302(detail["qqdocurl"])
+            yield Text(f"[哔哩哔哩] {detail['desc']}\n{url}")
+            yield Image(url=detail["preview"])
+
+        yield default()
 
     @override
     async def convert_segment(
@@ -46,7 +72,10 @@ class MessageProcessor(BaseMessageProcessor[MessageSegment, Bot, Message]):
             case "forward":
                 yield Text(f"[合并转发:{segment.data['id']}]")
             case "json":
-                yield self.handle_json_msg(segment.data)
+                with contextlib.suppress(Exception):
+                    json_data = json.loads(segment.data["data"])
+                    async for seg in self.handle_json_msg(json_data):
+                        yield seg
             case _:
                 async for seg in super().convert_segment(segment):
                     yield seg
