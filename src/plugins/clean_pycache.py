5a645d59cf8e81b4dc81569a3fe2f6ad7a7af3c0
wyf7685 <wyf7685@163.com>
2024-12-03 14:57:32 +0800

    重命名插件为 clean_pycache，修改描述以反映在驱动启动时清理 __pycache__ 的功能

--- src/plugins/clean_pycache.py
+++ src/plugins/clean_pycache.py
@@ -2,8 +2,8 @@ from nonebot import get_driver
 from nonebot.plugin import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
-    name="on_driver_startup",
-    description="Start jobs on driver startup",
+    name="clean_pycache",
+    description="Clean __pycache__ on driver startup",
     usage="None",
     type="application",
     supported_adapters=None,
@@ -16,8 +16,7 @@ def clean_pycache() -> None:
     from queue import Queue
     from shutil import rmtree
 
-    que = Queue[Path]()
-    (put := que.put)(Path.cwd())
+    (put := (que := Queue[Path]()).put)(Path.cwd())
     while not que.empty():
         for p in filter(Path.is_dir, que.get().iterdir()):
             (rmtree if p.name == "__pycache__" else put)(p)
