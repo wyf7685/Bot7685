600a96ab487749c331dbce48bd6db8e3cc259c3e
wyf7685 <wyf7685@163.com>
2024-12-16 12:30:23 +0800

    custom meow word

--- src/plugins/hooks/meow.py
+++ src/plugins/hooks/meow.py
@@ -6,19 +6,25 @@ from nonebot.adapters.onebot.v11 import Bot, Message, MessageSegment
 
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_localstore")
-from nonebot_plugin_alconna import Alconna, CommandMeta, Subcommand, on_alconna
+from nonebot_plugin_alconna import Alconna, Args, CommandMeta, Subcommand, on_alconna
 from nonebot_plugin_localstore import get_plugin_config_file
 
-conf = get_plugin_config_file("meow.enabled").resolve()
-enabled = conf.exists()
+enabled_flag = get_plugin_config_file("meow.enabled").resolve()
+enabled = enabled_flag.exists()
+word_file = get_plugin_config_file("meow.txt").resolve()
+if not word_file.exists():
+    word_file.write_text(meow_word := "喵")
+else:
+    meow_word = word_file.read_text().strip()
 
 toggle = on_alconna(
     Alconna(
         "meow",
         Subcommand("enable", alias={"e"}, help_text="启用"),
         Subcommand("disable", alias={"d"}, help_text="禁用"),
+        Subcommand("set", Args["word#内容", str], alias={"s"}, help_text="设置"),
         meta=CommandMeta(
-            description="meow",
+            description="meow?",
             usage="/meow <enable|disable>",
         ),
     )
@@ -30,7 +36,7 @@ def handle_meow_enable() -> None:
     global enabled
 
     if not enabled:
-        conf.touch()
+        enabled_flag.touch()
         enabled = True
 
 
@@ -39,8 +45,16 @@ def handle_meow_disable() -> None:
     global enabled
 
     if enabled:
-        conf.unlink()
+        enabled_flag.unlink()
         enabled = False
+
+
+@toggle.assign("set")
+async def handle_meow_set(word: str) -> None:
+    global meow_word
+
+    word_file.write_text(word)
+    meow_word = word
 
 
 @BaseBot.on_calling_api
@@ -65,11 +79,11 @@ async def _(bot: BaseBot, api: str, data: dict[str, Any]) -> None:
         and isinstance(seg := message[-1], MessageSegment)
         and seg.type == "text"
     ):
-        seg.data["text"] += "喵"
+        seg.data["text"] += meow_word
     elif (
         isinstance(message, list)
         and message
         and isinstance(seg := message[-1], dict)
         and seg.get("type") == "text"
     ):
-        seg["data"]["text"] += "喵"
+        seg["data"]["text"] += meow_word
