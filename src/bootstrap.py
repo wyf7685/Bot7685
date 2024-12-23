3a099c40ceee7184e135db0458c908d0e640e4cc
wyf7685 <wyf7685@163.com>
2024-12-23 15:55:11 +0800

    重构配置和插件加载逻辑，移至新文件 `src/bootstrap.py`

---
+++ src/bootstrap.py
@@ -0,0 +1,71 @@
+import importlib
+import logging
+import pathlib
+import tomllib
+from typing import Any
+
+import nonebot
+import yaml
+
+
+def setup_logger() -> None:
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    nonebot.logger.add(
+        "./logs/{time:YYYY-MM-DD}.log",
+        rotation="00:00",
+        level="DEBUG",
+        format=(
+            "<g>{time:HH:mm:ss}</g> "
+            "[<lvl>{level}</lvl>] "
+            "<c><u>{name}</u></c> | "
+            "<c>{function}:{line}</c> | "
+            "{message}"
+        ),
+    )
+    nonebot.logger.add(
+        "./logs/{time:YYYY-MM-DD}.colorize.log",
+        rotation="00:00",
+        level="DEBUG",
+        colorize=True,
+        diagnose=True,
+        format=(
+            "<g>{time:HH:mm:ss}</g> "
+            "[<lvl>{level}</lvl>] "
+            "<c><u>{name}</u></c> | "
+            "<c>{function}:{line}</c> | "
+            "{message}"
+        ),
+    )
+
+
+def load_config() -> dict[str, Any]:
+    config_file = pathlib.Path("config.yaml")
+    if not config_file.exists():
+        return {}
+    config: dict[str, Any] = yaml.safe_load(config_file.open("r"))
+    config |= config.pop(config.get("environment", "prod"), {})
+    return config
+
+
+def load_plugins() -> None:
+    nonebot_data: dict = (
+        tomllib.loads(pathlib.Path("pyproject.toml").read_text("utf-8"))
+        .get("tool", {})
+        .get("nonebot")
+    )
+
+    for item in nonebot_data.get("adapters", []):
+        module = importlib.import_module(item["module_name"])
+        nonebot.get_driver().register_adapter(module.Adapter)
+
+    plugins: list[str] = nonebot_data.get("plugins", [])
+    for p in pathlib.Path("src/dev").iterdir():
+        if (p.is_dir() and (name := p.name) in plugins) or (
+            p.is_file() and p.suffix == ".py" and (name := p.stem) in plugins
+        ):
+            plugins.remove(name)
+            nonebot.logger.opt(colors=True).warning(
+                f'优先加载来自 "<m>src.dev.{name}</m>" 的插件 "<y>{name}</y>"'
+            )
+
+    nonebot.load_all_plugins(plugins, ["src/plugins", "src/dev"])
