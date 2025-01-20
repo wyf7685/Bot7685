5e60f734b072cbec235b6c0305c0a792eb0684ff
wyf7685 <wyf7685@163.com>
2025-01-20 13:02:30 +0800

    update

--- src/plugins/no_shit.py
+++ src/plugins/no_shit.py
@@ -1,8 +1,11 @@
+from typing import Annotated
+
 import anyio
 from nonebot import require
 from nonebot.adapters.onebot.v11 import Bot
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
-from nonebot.typing import T_State
+from nonebot.matcher import Matcher
+from nonebot.params import Depends
 
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_waiter")
@@ -13,42 +16,44 @@ from nonebot_plugin_waiter import waiter
 require("src.plugins.trusted")
 from src.plugins.trusted import TrustedUser
 
-
-async def check_reply(event: GroupMessageEvent, state: T_State) -> bool:
-    if event.reply is None:
-        return False
-
-    if (target := event.reply.sender.user_id) == event.user_id:
-        return False
-
-    state["reply"] = event.reply.message_id
-    state["target_id"] = target
-    return target is not None
+no_shit = on_alconna("他在搬史", permission=TrustedUser)
 
 
-no_shit = on_alconna("他在搬史", permission=TrustedUser, rule=check_reply)
+async def check_reply(event: GroupMessageEvent) -> tuple[int, int]:
+    if (
+        event.reply is None
+        or (target := event.reply.sender.user_id) is None
+        or target == event.user_id
+    ):
+        Matcher.skip()
+
+    return event.reply.message_id, target
+
+
+ReplyInfo = Annotated[tuple[int, int], Depends(check_reply)]
 
 
 @no_shit.handle()
-async def _(bot: Bot, event: GroupMessageEvent, state: T_State) -> None:
-    target: int = state["target_id"]
+async def _(bot: Bot, event: GroupMessageEvent, r: ReplyInfo) -> None:
+    reply, target = r
 
     await (
-        UniMessage.reply(str(state["reply"]))
+        UniMessage.reply(str(reply))
         .at(str(target))
-        .text("被指控搬史\n")
+        .text(" 被指控搬史\n")
         .text("预期禁言 1 分钟\n\n")
         .text("若3分钟内有2人发送“同意”将实施禁言")
         .send()
     )
 
     voted: set[int] = set()
+    should_ban = False
 
     def _rule(e: GroupMessageEvent) -> bool:
         return (
             e.group_id == event.group_id
-            and e.user_id not in ({event.user_id, target} | voted)
-            and e.get_message().extract_plain_text() == "同意"
+            and e.user_id not in {*voted, event.user_id}
+            and e.get_message().extract_plain_text().strip() == "同意"
         )
 
     @waiter(
@@ -62,18 +67,16 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State) -> None:
 
     with anyio.move_on_after(60 * 3):
         async for user in wait():
+            if should_ban := user == target:
+                break
             if user is not None:
                 voted.add(user)
-            if len(voted) >= 2:
+            if should_ban := len(voted) >= 2:
                 break
 
-    if len(voted) < 2:
-        await UniMessage.text("同意人数不足，取消操作").finish()
+    if not should_ban:
+        await UniMessage.text("同意人数不足，取消操作").finish(reply_to=True)
 
-    await bot.delete_msg(message_id=state["reply"])
-    await bot.set_group_ban(
-        group_id=event.group_id,
-        user_id=target,
-        duration=60,
-    )
-    await UniMessage.at(str(target)).text("已被禁言").finish()
+    await bot.delete_msg(message_id=reply)
+    await bot.set_group_ban(group_id=event.group_id, user_id=target, duration=60)
+    await UniMessage.at(str(target)).text(" 已被禁言").finish(reply_to=True)
