02f4bbe71874755586262744991b61708f14d492
wyf7685 <wyf7685@163.com>
2025-02-23 15:32:23 +0800

    patch driver.request

--- src/plugins/hooks/tmp_fix_ntqq_media.py
+++ src/plugins/hooks/tmp_fix_ntqq_media.py
@@ -1,13 +1,37 @@
-import itertools
+import contextlib
+from typing import Protocol, override
 
-from nonebot.adapters.onebot.v11 import MessageEvent
-from nonebot.message import event_preprocessor
+from nonebot import get_driver
+from nonebot.drivers import HTTPClientMixin, Request, Response
 
 
-@event_preprocessor
-async def _(event: MessageEvent) -> None:
-    for seg in itertools.chain(event.message, event.original_message):
-        if seg.type == "image":
-            url: str = seg.data.get("url", "")
-            if "https://multimedia.nt.qq.com.cn" in url:
-                seg.data["url"] = url.replace("https://multimedia", "http://multimedia")
+class _RequestCall[M: HTTPClientMixin](Protocol):
+    async def __call__(  # sourcery skip: instance-method-first-arg-name
+        self_,  # pyright:ignore[reportSelfClsParameterName]  # noqa: N805
+        self: M,
+        setup: Request,
+    ) -> Response: ...
+
+
+def patch_request[M: HTTPClientMixin](original: _RequestCall[M]) -> _RequestCall[M]:
+    @override
+    async def request(self: M, setup: Request) -> Response:
+        if setup.url.host == "multimedia.nt.qq.com.cn":
+            setup.url.scheme = "http"
+
+        return await original(self, setup)
+
+    return request
+
+
+with contextlib.suppress(ImportError):
+    from nonebot.drivers.aiohttp import Mixin as AIOHTTPMixin
+
+    if issubclass(cls := get_driver().__class__, AIOHTTPMixin):
+        cls.request = patch_request(cls.request)
+
+with contextlib.suppress(ImportError):
+    from nonebot.drivers.httpx import Mixin as HTTPXMixin
+
+    if issubclass(cls := get_driver().__class__, HTTPXMixin):
+        cls.request = patch_request(cls.request)
