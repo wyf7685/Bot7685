b5b59ab8ab47360bde9fd5e3d1cc0e45c87762c6
wyf7685 <wyf7685@163.com>
2025-02-24 17:34:39 +0800

    update

--- src/plugins/group_pipe/database/pipe.py
+++ src/plugins/group_pipe/database/pipe.py
@@ -1,11 +1,13 @@
 import functools
-from collections.abc import Sequence
-from typing import NamedTuple
+from collections.abc import Iterable
+from typing import TYPE_CHECKING, NamedTuple
 
 from nonebot_plugin_alconna import Target
 from nonebot_plugin_orm import Model, get_scoped_session
 from sqlalchemy import JSON, Integer, delete, select
 from sqlalchemy.orm import Mapped, mapped_column
+
+type TargetDict = dict[str, object]
 
 
 class PipeTuple(NamedTuple):
@@ -13,25 +15,28 @@ class PipeTuple(NamedTuple):
     target: Target
 
     @classmethod
-    def from_scalars(cls, pipes: Sequence[tuple[dict, dict]]) -> list["PipeTuple"]:
+    def from_scalars(
+        cls, pipes: Iterable[tuple[TargetDict, TargetDict]]
+    ) -> list["PipeTuple"]:
         return [
             cls(Target.load(listen), Target.load(target)) for listen, target in pipes
         ]
 
 
-@functools.cache
-def make_key(target: Target) -> int:
+def make_key(target: Target, /) -> int:
     args = (target.id, target.channel, target.private, target.self_id)
-    if target.extra.get("scope"):
-        args += (target.extra["scope"],)
-    if target.extra.get("adapter"):
-        args += (str(target.extra["adapter"]),)
+    for k, v in target.extra.items():
+        args += (k, v)
     key = "".join(map(str, args)).encode("utf-8")
     result = 0
     for i in key:
-        result = (result << 5) - result + i
+        result = ((result << 5) - result + i)
         result &= 0xFFFFFFFFFFFF
     return result % (1 << 31)
+
+
+if not TYPE_CHECKING:
+    make_key = functools.lru_cache(maxsize=16)(make_key)
 
 
 class Pipe(Model):
@@ -39,9 +44,9 @@ class Pipe(Model):
     """ 监听群组 """
     target: Mapped[int] = mapped_column(Integer(), nullable=False, primary_key=True)
     """ 目标群组 """
-    listen_t: Mapped[dict] = mapped_column(JSON(), nullable=False)
+    listen_t: Mapped[TargetDict] = mapped_column(JSON(), nullable=False)
     """ 监听群组的 Target """
-    target_t: Mapped[dict] = mapped_column(JSON(), nullable=False)
+    target_t: Mapped[TargetDict] = mapped_column(JSON(), nullable=False)
     """ 目标群组的 Target """
 
     def get_listen(self) -> Target:
@@ -67,7 +72,7 @@ class PipeDAO:
         if target is not None:
             statement = statement.where(Pipe.target == make_key(target))
         result = await self.session.execute(statement)
-        return PipeTuple.from_scalars([row.tuple() for row in result.all()])
+        return PipeTuple.from_scalars(row.tuple() for row in result.all())
 
     async def get_linked_pipes(
         self, query: Target
@@ -76,11 +81,11 @@ class PipeDAO:
 
         statement = select(Pipe.listen_t, Pipe.target_t).where(Pipe.listen == key)
         result = await self.session.execute(statement)
-        listen_pipes = PipeTuple.from_scalars([row.tuple() for row in result.all()])
+        listen_pipes = PipeTuple.from_scalars(row.tuple() for row in result.all())
 
         statement = select(Pipe.listen_t, Pipe.target_t).where(Pipe.target == key)
         result = await self.session.execute(statement)
-        target_pipes = PipeTuple.from_scalars([row.tuple() for row in result.all()])
+        target_pipes = PipeTuple.from_scalars(row.tuple() for row in result.all())
 
         return listen_pipes, target_pipes
 
