7f5bba0df6a6a0211c1a0fff09a7b1dbe7aa8248
wyf7685 <wyf7685@163.com>
2025-03-06 16:22:34 +0800

    update no_shit

--- src/plugins/no_shit.py
+++ src/plugins/no_shit.py
@@ -1,3 +1,5 @@
+import json
+
 import anyio
 from nonebot import require
 from nonebot.adapters.onebot.v11 import Bot
@@ -6,26 +8,29 @@ from nonebot.matcher import Matcher
 from nonebot.params import Depends
 
 require("nonebot_plugin_alconna")
+require("nonebot_plugin_htmlrender")
 require("nonebot_plugin_localstore")
 require("nonebot_plugin_waiter")
 from nonebot_plugin_alconna import on_alconna
 from nonebot_plugin_alconna.uniseg import UniMessage
-from nonebot_plugin_localstore import get_plugin_data_dir
+from nonebot_plugin_htmlrender import md_to_pic
+from nonebot_plugin_localstore import get_plugin_data_file
 from nonebot_plugin_waiter import waiter
 
 require("src.plugins.trusted")
 from src.plugins.trusted import TrustedUser
 
 
-def add_ban_count(user_id: int) -> int:
-    file = get_plugin_data_dir() / "count" / f"{user_id}"
-    file.parent.mkdir(parents=True, exist_ok=True)
-    cnt = int(file.read_text()) + 1 if file.exists() else 1
-    file.write_text(str(cnt))
+def add_ban_count(group_id: int, user_id: int) -> int:
+    file = get_plugin_data_file(f"{group_id}.json")
+    data: dict[str, int] = json.loads(file.read_text())
+    cnt = data[str(user_id)] = data.get(str(user_id), 0) + 1
+    file.write_text(json.dumps(data))
     return cnt
 
 
 no_shit = on_alconna("他在搬史", permission=TrustedUser)
+shit_rank = on_alconna("搬史榜", permission=TrustedUser)
 
 
 async def check_reply(event: GroupMessageEvent) -> tuple[int, int]:
@@ -89,9 +94,30 @@ async def _(
 
     await bot.delete_msg(message_id=reply)
     await bot.set_group_ban(group_id=event.group_id, user_id=target, duration=60)
-    cnt = add_ban_count(target)
+    cnt = add_ban_count(event.group_id, target)
     await (
         UniMessage.at(str(target))
         .text(f" 已被禁言, 共计 {cnt} 次")
         .finish(reply_to=True)
     )
+
+
+@shit_rank.handle()
+async def _(event: GroupMessageEvent) -> None:
+    file = get_plugin_data_file(f"{event.group_id}.json")
+    if not file.exists():
+        await UniMessage.text("暂无记录").finish()
+
+    data: dict[str, int] = json.loads(file.read_text())
+
+    table = "<table>"
+    for user_id, cnt in sorted(data.items(), key=lambda x: x[1], reverse=True):
+        avatar_url = f"http://thirdqq.qlogo.cn/headimg_dl?dst_uin={user_id}&spec=640"
+        avatar = f'<td><picture><img src="{avatar_url}" /></picture></td>'
+        text = f"<td>{user_id}<br/>共计 {cnt} 次</td>"
+        table += f"<tr>{avatar}{text}</tr>"
+    table += "</table>"
+
+    md = "## 搬史榜\n\n" + table
+    pic = await md_to_pic(md)
+    await UniMessage.image(raw=pic).send()
