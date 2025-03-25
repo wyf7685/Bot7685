57b3d777224792006b342593432ee42461ab0999
wyf7685 <wyf7685@163.com>
2025-03-25 23:05:58 +0800

    update

--- src/plugins/jmcomic/__init__.py
+++ src/plugins/jmcomic/__init__.py
@@ -1,14 +1,20 @@
 from nonebot import require
+from nonebot.adapters.onebot import v11
+from nonebot.adapters.onebot.v11 import Message as V11Msg
+from nonebot.adapters.onebot.v11 import MessageSegment as V11Seg
+from nonebot.params import Depends
 from nonebot.plugin import PluginMetadata
-from nonebot.utils import run_sync
-
-import jmcomic
 
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_localstore")
 from nonebot_plugin_alconna import Alconna, Args, UniMessage, on_alconna
 
-from .jm_option import PDF_DIR, option
+from .jm_option import (
+    DOWNLOAD_DIR,
+    download_album,
+    download_album_pdf,
+    get_album_detail,
+)
 
 __plugin_meta__ = PluginMetadata(
     name="jmcomic",
@@ -20,9 +26,52 @@ __plugin_meta__ = PluginMetadata(
 matcher = on_alconna(Alconna("jm", Args["album_id", int]))
 
 
+async def check_lagrange(bot: v11.Bot) -> None:
+    info = await bot.get_version_info()
+    if "lagrange" not in str(info.get("app_name", "unkown")).lower():
+        matcher.skip()
+
+
+@matcher.assign("album_id", parameterless=[Depends(check_lagrange)])
+async def _(
+    bot: v11.Bot,
+    event: v11.PrivateMessageEvent | v11.GroupMessageEvent,
+    album_id: int,
+) -> None:
+    detail = await get_album_detail(album_id)
+    await matcher.send(
+        V11Seg.text(
+            f"标题：{detail.title}\n"
+            f"作者：{detail.author}\n"
+            f"标签：{', '.join(detail.tags)}\n"
+            f"页数：{detail.page_count}\n"
+        )
+    )
+
+    fwds: list[list[V11Seg]] = []
+    nodes: list[V11Seg] = []
+
+    await download_album(album_id)
+    for idx, file in enumerate((DOWNLOAD_DIR / str(album_id)).iterdir(), 1):
+        msg = V11Msg(V11Seg.image(file.read_bytes()))
+        node = V11Seg.node_custom(10086, f"P {idx}", msg)
+        nodes.append(node)
+        if len(nodes) >= 50:
+            fwds.append(nodes.copy())
+            nodes.clear()
+    if nodes:
+        fwds.append(nodes.copy())
+
+    for nodes in fwds:
+        if isinstance(event, v11.GroupMessageEvent):
+            await bot.send_group_forward_msg(group_id=event.group_id, messages=nodes)
+        else:
+            await bot.send_private_forward_msg(user_id=event.user_id, messages=nodes)
+
+    await matcher.finish()
+
+
 @matcher.assign("album_id")
 async def _(album_id: int) -> None:
-    downloader = jmcomic.new_downloader(option)
-    detail = await run_sync(downloader.download_album)(album_id)
-    pdf_file = PDF_DIR / f"{detail.album_id}.pdf"
-    await UniMessage.file(path=pdf_file).send()
+    async with download_album_pdf(album_id) as pdf_file:
+        await UniMessage.file(path=pdf_file).send()
--- src/plugins/jmcomic/jm_option.py
+++ src/plugins/jmcomic/jm_option.py
@@ -1,29 +1,29 @@
+import contextlib
+import pathlib
+from collections.abc import AsyncGenerator
+
+from nonebot.log import logger
+from nonebot.utils import escape_tag, run_sync
 from nonebot_plugin_localstore import get_plugin_cache_dir
 
-from jmcomic import JmOption
+import jmcomic
+
+logger = logger.opt(colors=True)
 
 
-def patch_log() -> None:
-    from jmcomic import JmModuleConfig
-
-    def log(_: object, topic: str, msg: str) -> None:
-        from nonebot import logger
-        from nonebot.utils import escape_tag
-
-        logger.opt(colors=True).info(f"[<m>{topic}</m>] {escape_tag(msg)}")
-
-    JmModuleConfig.jm_log.__func__.__code__ = log.__code__  # pyright:ignore[reportFunctionMemberAccess]
+def jm_log(topic: str, msg: str) -> None:
+    logger.info(f"[<m>{topic}</m>] {escape_tag(msg)}")
 
 
 CACHE_DIR = get_plugin_cache_dir()
 DOWNLOAD_DIR = CACHE_DIR / "download"
 PDF_DIR = CACHE_DIR / "pdf"
-option_dict = {
+OPTION = {
     "version": "2.1",
-    "dir_rule": {
-        "base_dir": str(DOWNLOAD_DIR),
-    },
+    "dir_rule": {"base_dir": str(DOWNLOAD_DIR), "rule": "Bd_Pid"},
     "download": {"threading": {"image": 4, "photo": 4}},
+}
+OPTION_PDF = OPTION | {
     "plugins": {
         "after_album": [
             {
@@ -37,5 +37,26 @@ option_dict = {
     },
 }
 
-patch_log()
-option = JmOption.construct(option_dict)
+jmcomic.JmModuleConfig.EXECUTOR_LOG = jm_log  # pyright: ignore[reportAttributeAccessIssue]
+option = jmcomic.JmOption.construct(OPTION)
+option_pdf = jmcomic.JmOption.construct(OPTION_PDF)
+
+
+@contextlib.asynccontextmanager
+async def download_album_pdf(album_id: int) -> AsyncGenerator[pathlib.Path]:
+    downloader = jmcomic.new_downloader(option_pdf)
+    detail = await run_sync(downloader.download_album)(album_id)
+    pdf_file = PDF_DIR / f"{detail.album_id}.pdf"
+    try:
+        yield pdf_file
+    finally:
+        pdf_file.unlink()
+
+
+async def get_album_detail(album_id: int) -> jmcomic.JmAlbumDetail:
+    return await run_sync(option.new_jm_client().get_album_detail)(album_id)
+
+
+async def download_album(album_id: int) -> jmcomic.JmAlbumDetail:
+    downloader = jmcomic.new_downloader(option)
+    return await run_sync(downloader.download_album)(album_id)
