c575a2475cc8458eeec0933bbd4e6f41eb7a0af5
wyf7685 <wyf7685@163.com>
2025-04-05 21:41:28 +0800

    fix

--- src/plugins/random_neuro.py
+++ src/plugins/random_neuro.py
@@ -1,6 +1,5 @@
 import random
 from pathlib import Path
-from typing import NoReturn
 
 from nonebot import require
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters
@@ -16,17 +15,16 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
 )
 
-images = list((Path()).iterdir())
 image_dir = Path("data/random_neuro")
 neuro = list((image_dir / "neuro").iterdir())
 evil = list((image_dir / "evil").iterdir())
 
 
-@(Command("随机neuro [...args]").build(priority=2).handle())
-async def cmd_neuro() -> NoReturn:
+@(Command("随机neuro [...args]").build(priority=10).handle())
+async def cmd_neuro() -> None:
     await UniMessage.image(raw=random.choice(neuro).read_bytes()).finish(reply_to=True)
 
 
-@(Command("随机evil [...args]").build(priority=2).handle())
-async def cmd_evil() -> NoReturn:
+@(Command("随机evil [...args]").build(priority=10).handle())
+async def cmd_evil() -> None:
     await UniMessage.image(raw=random.choice(evil).read_bytes()).finish(reply_to=True)
