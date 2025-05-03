3a7dc0575f1fbcfbae756a09c526a7218c9edc1b
wyf7685 <wyf7685@163.com>
2025-05-03 20:54:23 +0800

    orm

--- bot.py
+++ bot.py
@@ -1,8 +1,27 @@
+import sys
+from typing import NoReturn
+
 from src.bootstrap import init_nonebot
 
 app = init_nonebot()
 
-if __name__ == "__main__":
+
+def run_orm_upgrade() -> NoReturn:
+    import asyncio
+
+    from nonebot_plugin_orm import init_orm
+
+    sys.exit(asyncio.run(init_orm()))  # pyright:ignore[reportArgumentType]
+
+
+def main() -> None:
+    if "orm-upgrade" in sys.argv:
+        run_orm_upgrade()
+
     import nonebot
 
-    nonebot.run()
+    sys.exit(nonebot.run())
+
+
+if __name__ == "__main__":
+    main()
