c922444cf209fa4a95ff3efad222d539d7eaf311
wyf7685 <wyf7685@163.com>
2025-05-04 21:43:46 +0800

    use waiter

--- src/plugins/friend_add.py
+++ src/plugins/friend_add.py
@@ -1,13 +1,14 @@
 import contextlib
 
-import anyio
-from nonebot import get_driver, on_message, on_type, require
+from nonebot import get_driver, on_type, require
 from nonebot.adapters.onebot.v11 import Bot, FriendRequestEvent, PrivateMessageEvent
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import PluginMetadata
 
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_uninfo")
+require("nonebot_plugin_waiter")
+import nonebot_plugin_waiter as waiter
 from nonebot_plugin_alconna.uniseg import Receipt, Reply, Target, UniMessage, UniMsg
 from nonebot_plugin_uninfo import Uninfo
 
@@ -21,7 +22,7 @@ __plugin_meta__ = PluginMetadata(
 
 
 @on_type(FriendRequestEvent).handle()
-async def handle(event: FriendRequestEvent, info: Uninfo) -> None:
+async def handle(bot: Bot, event: FriendRequestEvent, info: Uninfo) -> None:
     name = info.user.nick or info.user.name or info.user.id
     message = UniMessage.text(f"收到好友申请: {name}({info.user.id})\n")
     if avatar := info.user.avatar:
@@ -34,9 +35,8 @@ async def handle(event: FriendRequestEvent, info: Uninfo) -> None:
         if adapter and adapter != "onebot":
             continue
 
-        target = Target.user(user)
         with contextlib.suppress(Exception):
-            receipts[user_id] = await message.send(target)
+            receipts[user_id] = await Target.user(user).send(message)
 
     if not receipts:
         return
@@ -46,32 +46,22 @@ async def handle(event: FriendRequestEvent, info: Uninfo) -> None:
             (receipt := receipts.get(event.get_user_id())) is not None
             and (reply := receipt.get_reply()) is not None
             and len(reply) > 0
-            and (r := reply[0])
             and msg.has(Reply)
-            and msg[Reply, 0].id == r.id
+            and msg[Reply, 0].id == reply[0].id
             and msg.extract_plain_text() in {"接受", "拒绝"}
         )
 
-    async def handler(bot: Bot, msg: UniMsg) -> None:
-        await (
-            event.approve
-            if (text := msg.extract_plain_text()) == "接受"
-            else event.reject
-        )(bot)
-        await UniMessage.text(f"已{text}该好友申请").send()
-        finished.set()
-
-    matcher = on_message(
+    @waiter.waiter(
+        [PrivateMessageEvent],
+        keep_session=False,
         rule=rule,
         permission=SUPERUSER,
-        handlers=[handler],
-        temp=True,
     )
-    finished = anyio.Event()
-    with anyio.move_on_after(10 * 60):
-        await finished.wait()
+    def wait(event: PrivateMessageEvent) -> bool:
+        return event.get_message().extract_plain_text() == "接受"
 
-    if not finished.is_set():
-        matcher.destroy()
+    if (res := await wait.wait(timeout=10 * 60)) is not None:
+        await (event.approve if res else event.reject)(bot)
+    else:
         for receipt in receipts.values():
             await receipt.reply("操作超时，将忽略该好友申请")
