4a7cac48c0cbf2799151c91ea94bd9f8e73edb27
wyf7685 <wyf7685@163.com>
2025-06-10 14:49:20 +0800

    update

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
-import sys
-from typing import NoReturn
-
 from src.bootstrap import init_nonebot
 
 app = init_nonebot()
 
 
-def run_orm_upgrade() -> NoReturn:
-    import asyncio
-
-    from src.utils import orm_upgrade
-
-    sys.exit(asyncio.run(orm_upgrade()))
-
-
-def main() -> None:
-    if "orm-upgrade" in sys.argv:
-        run_orm_upgrade()
-
-    import nonebot
-
-    sys.exit(nonebot.run())
-
-
 if __name__ == "__main__":
-    main()
+    import sys
+
+    if "orm-upgrade" not in sys.argv:
+        import nonebot
+
+        nonebot.run()
+    else:
+        import asyncio
+
+        from src.utils import orm_upgrade
+
+        asyncio.run(orm_upgrade())
--- src/plugins/broken_pic.py
+++ src/plugins/broken_pic.py
@@ -1,5 +1,5 @@
 import random
-from typing import override
+from typing import ClassVar, override
 
 from nonebot import require
 from nonebot.adapters import Bot, Event, Message
@@ -12,6 +12,8 @@ from nonebot_plugin_localstore import get_plugin_data_dir
 
 
 class BrokenPicExtension(Extension):
+    probability: ClassVar[float] = 0.001
+
     @property
     @override
     def priority(self) -> int:
@@ -37,7 +39,7 @@ class BrokenPicExtension(Extension):
         send = send.copy()
 
         for idx, segment in enumerate(send[:]):
-            if isinstance(segment, Image) and random.random() < 0.0001:
+            if isinstance(segment, Image) and random.random() < self.probability:
                 file = random.choice(list(get_plugin_data_dir().iterdir()))
                 send[idx] = Image(raw=file.read_bytes())
 
