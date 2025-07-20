2ab048f657dd0c2f935754e5ede468b42ea58cfb
wyf7685 <wyf7685@163.com>
2025-07-20 15:00:56 +0800

    update

--- src/plugins/hooks/patch_wordcloud.py
+++ src/plugins/hooks/patch_wordcloud.py
@@ -1,24 +1,22 @@
 # ruff: noqa: SLF001
-
-from nonebot import require
-
-require("nonebot_plugin_wordcloud")
 import nonebot
-import nonebot_plugin_wordcloud.data_source as source
-
-original = source._get_wordcloud  # pyright: ignore[reportPrivateUsage]
-
-
-def _get_wordcloud(messages: list[str], mask_key: str) -> bytes | None:
-    gen = (m for m in messages if "当前版本不支持该消息类型" not in m)
-    return original(gen, mask_key)  # pyright: ignore[reportArgumentType]
 
 
 @nonebot.get_driver().on_startup
-def patch() -> None:
+def _() -> None:
+    try:
+        nonebot.require("nonebot_plugin_wordcloud")
+        import nonebot_plugin_wordcloud.data_source as source
+    except (ImportError, RuntimeError):
+        return
+
+    def _get_wordcloud(messages: list[str], mask_key: str) -> bytes | None:
+        gen = (m for m in messages if "当前版本不支持该消息类型" not in m)
+        return original(iter(gen), mask_key)  # pyright: ignore[reportArgumentType]
+
+    original = source._get_wordcloud  # pyright: ignore[reportPrivateUsage]
     source._get_wordcloud = _get_wordcloud  # pyright: ignore[reportPrivateUsage]
 
-
-@nonebot.get_driver().on_shutdown
-def dispose() -> None:
-    source._get_wordcloud = original  # pyright: ignore[reportPrivateUsage]
+    @nonebot.get_driver().on_shutdown
+    def _() -> None:
+        source._get_wordcloud = original
