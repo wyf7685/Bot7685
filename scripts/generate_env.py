3cdf54d0b06a3dd0c76af7e9fba64d28b8aa53b8
wyf7685 <wyf7685@163.com>
2025-08-23 20:26:31 +0800

    update script

--- scripts/generate_env.py
+++ scripts/generate_env.py
@@ -4,19 +4,20 @@
 
 import contextlib
 import json
-import pathlib
-from collections.abc import Generator
+import uuid
+from collections.abc import Callable, Generator
+from pathlib import Path
 from typing import Any, cast
 
 from msgspec import toml as msgtoml
 from msgspec import yaml as msgyaml
 
-root = pathlib.Path(__file__).resolve().parent.parent
+root = Path(__file__).resolve().parent.parent
 env_file = root / ".env"
 toml_file = root / "pyproject.toml"
 
 
-def _load_yaml(file_path: pathlib.Path) -> dict[str, Any]:
+def _load_yaml(file_path: Path) -> dict[str, Any]:
     data = msgyaml.decode(file_path.read_bytes()) or {}
 
     if data.pop("scope_compat", None):
@@ -48,61 +49,89 @@ def deep_update(
     return updated_mapping
 
 
-def load_config() -> dict[str, Any]:
-    config_dir = pathlib.Path("config")
-    root_config = config_dir / "config.yaml"
-    if not root_config.exists():
+def _find_config_file(fp: Path) -> Path | None:
+    for suffix in ".toml", ".yaml", ".yml", ".json":
+        if (file := fp.with_suffix(suffix)).exists():
+            return file
+    return None
+
+
+_decoders: dict[str, Callable[[bytes], dict[str, Any]]] = {}
+
+
+def _get_decoder(suffix: str) -> Callable[[bytes], dict[str, Any]] | None:
+    if suffix not in _decoders:
+        match suffix:
+            case ".toml":
+                from msgspec import toml as module
+            case ".yaml" | ".yml":
+                from msgspec import yaml as module
+            case ".json":
+                from msgspec import json as module
+            case _:
+                return None
+        _decoders[suffix] = module.decode
+
+    return _decoders[suffix]
+
+
+def _load_file(fp: Path) -> dict[str, Any]:
+    if (decoder := _get_decoder(fp.suffix)) is None:
         return {}
 
-    config: dict[str, object] = _load_yaml(root_config)
+    data = decoder(fp.read_bytes()) or {}
+
+    if data.pop("scope_compat", None):
+        for key, value in list(data.items()):
+            if isinstance(value, dict):
+                del data[key]
+                for k, v in value.items():
+                    data[f"{key}_{k}"] = v
+
+    return data
+
+
+def load_config() -> dict[str, Any]:
+    config_dir = Path("config")
+    if (root_config := _find_config_file(config_dir / "config")) is None:
+        return {}
+
+    config = _load_file(root_config)
 
     env = str(config.get("environment", "prod"))
-    env_config = config_dir / f"{env}.yaml"
-    if not env_config.exists():
+    if (env_config := _find_config_file(config_dir / env)) is None:
         return config
 
-    config = deep_update(config, _load_yaml(env_config))
+    config = deep_update(config, _load_file(env_config))
 
-    env_dir = config_dir / env
-    if not env_dir.exists():
-        return config
-
-    for p in env_dir.iterdir():
-        if p.suffix == ".yaml":
-            config = deep_update(config, _load_yaml(p))
+    if (env_dir := config_dir / env).exists():
+        for p in filter(Path.is_file, env_dir.iterdir()):
+            config = deep_update(config, _load_file(p))
 
     return config
 
 
-def generate_env_file(config: dict[str, object]) -> None:
+def generate_env_file(config: dict[str, Any]) -> None:
     env = "\n".join(f"{key}={json.dumps(value)}" for key, value in config.items())
     env_file.write_text(env)
 
 
-def generate_cli_toml(config: dict[str, object]) -> None:
+def generate_cli_toml(bs_cfg: dict[str, Any]) -> None:
     adapters = [
         {
             "name": adapter.replace(".", " ").replace("~", ""),
             "module_name": adapter.replace("~", "nonebot.adapters."),
         }
-        for adapter in cast("list[str]", config["adapters"])
+        for adapter in cast("list[str]", bs_cfg["adapters"])
     ]
-    plugins = cast("list[str]", config["plugins"])
-    plugin_dirs = [
-        config["plugin_dir"],
-        config["dev_plugin_dir"],
-    ]
-    for path in pathlib.Path(str(config["dev_plugin_dir"])).iterdir():
-        if path.is_dir() and path.name in plugins:
-            plugins.remove(path.name)
+    plugins = cast("list[str]", bs_cfg["plugins"])
 
     toml: dict[str, dict[str, object]] = msgtoml.decode(toml_file.read_text())
-    if "tool" not in toml:
-        toml["tool"] = {}
+    toml.setdefault("tool", {})
     toml["tool"]["nonebot"] = {
         "adapters": adapters,
         "plugins": plugins,
-        "plugin_dirs": plugin_dirs,
+        "plugin_dirs": bs_cfg["plugin_dirs"],
         "builtin_plugins": [],
     }
     toml_file.write_bytes(msgtoml.encode(toml))
@@ -110,17 +139,26 @@ def generate_cli_toml(config: dict[str, object]) -> None:
 
 @contextlib.contextmanager
 def ensure_cli() -> Generator[None]:
-    toml = toml_file.read_text()
+    u = str(uuid.uuid4()).partition("-")[0]
+    toml_backup = root / f"pyproject.{u}.toml"
+    toml_backup.write_bytes(toml_file.read_bytes())
     config = load_config()
-    generate_env_file(config)
-    generate_cli_toml(config["bootstrap"])
 
     try:
+        generate_env_file(config)
+        generate_cli_toml(config["bootstrap"])
         yield
     finally:
-        env_file.unlink()
-        toml_file.write_text(toml)
+        env_file.unlink(missing_ok=True)
+        toml_file.unlink(missing_ok=True)
+        toml_backup.rename(toml_file)
 
 
 if __name__ == "__main__":
-    generate_env_file(load_config())
+    import sys
+
+    if sys.argv[-1] == "cli":
+        with ensure_cli():
+            input("Press Enter to exit...")
+    else:
+        generate_env_file(load_config())
