d24bbda9f6a33f159fd8504f22406cb0278ac207
wyf7685 <wyf7685@163.com>
2025-09-11 00:19:49 +0800

    better reaction

--- src/plugins/hooks/reaction_before_matcher.py
+++ src/plugins/hooks/reaction_before_matcher.py
@@ -2,17 +2,29 @@ import contextlib
 
 from nonebot import get_driver, require
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent
-from nonebot.message import run_preprocessor
+from nonebot.message import run_postprocessor, run_preprocessor
 
 require("nonebot_plugin_alconna")
 from nonebot_plugin_alconna import message_reaction
 
+driver = get_driver()
 
-async def wrapper(bot: Bot, event: MessageEvent) -> None:
+
+async def wrapper(bot: Bot, event: MessageEvent, emoji: str) -> None:
     with contextlib.suppress(Exception):
-        await message_reaction("60", None, event, bot)
+        await message_reaction(emoji, None, event, bot)
 
 
 @run_preprocessor
 async def reaction_before_matcher(bot: Bot, event: MessageEvent) -> None:
-    get_driver().task_group.start_soon(wrapper, bot, event)
+    driver.task_group.start_soon(wrapper, bot, event, "60")  # coffee
+
+
+@run_postprocessor
+async def reaction_after_matcher(
+    bot: Bot,
+    event: MessageEvent,
+    exception: Exception | None,
+) -> None:
+    emoji = "144" if exception is None else "10060"  # 🎉 : ❌
+    driver.task_group.start_soon(wrapper, bot, event, emoji)
