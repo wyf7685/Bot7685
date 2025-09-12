6d228a477129d5e04002885eb747d9bffd599872
wyf7685 <wyf7685@163.com>
2025-09-12 23:28:04 +0800

    update entrypoint & workflow

--- docker/assets/entrypoint.sh
+++ docker/assets/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -d "${VIRTUAL_ENV}" ]; then
   uv venv "${VIRTUAL_ENV}"
 fi
-uv sync --locked --active --no-dev
+ALL_PROXY=${UV_SYNC_PROXY:-""} uv sync --locked --active --no-dev
 
 # Start Gunicorn
 gunicorn -k "uvicorn.workers.UvicornWorker" -c "/gunicorn_conf.py" "bot:app"
