8d923ae26ba3e856afbd1c3f567c52298d12e998
wyf7685 <wyf7685@163.com>
2026-03-04 18:46:31 +0800

    fix

--- src/plugins/patch_event/adapters/__init__.py
+++ src/plugins/patch_event/adapters/__init__.py
@@ -17,7 +17,7 @@ def __init() -> None:
     logger = nonebot.logger.opt(colors=True)
     for adapter in nonebot.get_adapters():
         if module := ADAPTERS.get(adapter):
-            logger.info(f"Patch event for adapter <g>{adapter}</>")
+            logger.info(f"Loading patchers for adapter <g>{adapter}</>")
             importlib.import_module(f"{__package__}.{module}")
 
 
