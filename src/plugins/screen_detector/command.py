10dccccd0aa0e3f915630e0739d86a8c20411a97
wyf7685 <wyf7685@163.com>
2026-05-31 21:07:24 +0800

    fix: handle network errors during file sending and log file size

--- src/plugins/screen_detector/command.py
+++ src/plugins/screen_detector/command.py
@@ -1,5 +1,7 @@
 from datetime import UTC, datetime, timedelta
 
+from nonebot import logger
+from nonebot.exception import NetworkError
 from nonebot_plugin_alconna import Alconna, Args, Subcommand, UniMessage, on_alconna
 
 from .api import detector_client
@@ -16,10 +18,13 @@ async def assign_package(duration: str) -> None:
     delta = timedelta(**{kwd: num})
 
     since = (datetime.now() - delta).astimezone(UTC)
-
     raw = await detector_client.package(since)
     if raw is None:
         await UniMessage.text("打包检测结果失败").finish()
+    logger.opt(colors=True).info(f"文件大小: <c>{len(raw) / 1024 / 1024:.3f}</> MB")
 
     filename = f"detector_package_{datetime.now():%Y-%m-%d_%H-%M-%S}.zip"
-    await UniMessage.file(raw=raw, name=filename).finish()
+    try:
+        await UniMessage.file(raw=raw, name=filename).send()
+    except NetworkError:
+        logger.warning("发送等待超时")
