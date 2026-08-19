4902062077fcaabf9f3173a36f2c9f19f0ef38c3
wyf7685 <wyf7685@163.com>
2026-08-19 17:40:42 +0800

    fix: patch htmlrender _connect

--- src/bootstrap/patches/__init__.py
+++ src/bootstrap/patches/__init__.py
@@ -1,3 +1,4 @@
+from . import htmlrender_connect as htmlrender_connect
 from . import pmn as pmn
 from .lifespan import create_patched_driver_class as create_patched_driver_class
 from .require import get_plugin_deps as get_plugin_deps
